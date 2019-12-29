f4a4c1add30513604f0a18e06a6255c643ed6420
avamisola <58097632+avamisola@users.noreply.github.com>
2019-12-29 12:33:17 -0500

    shirt design and color update
    shirt design and color update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//
+//treehouse fullstack javascript project 3: inteactive form
 
 //put name field in focus state
 $("#name").focus();
@@ -6,10 +6,34 @@ $("#name").focus();
 //show optional text input if "other" job role selected
 $("#other-title").hide();
 $("#title").change(function() {
-    if ($(this).val() == "other"){
+    if ($(this).val() == "other") {
         $("#other-title").show();
     } else {
         $("#other-title").hide();
+    }
+});
+
+//make shirt color options dependent on selected shirt design
+$("label[for='color']").text("Please select a T-shirt theme");
+$("#color").hide();
+$("#design").change(function() {
+    if ($(this).val() == "js puns") {
+        $("label[for='color']").text("Color:");
+        $("#color option").show();
+        $("#color").val("cornflowerblue").show();
+        $("#color option[value='tomato']").hide();
+        $("#color option[value='steelblue']").hide();
+        $("#color option[value='dimgrey']").hide();
+    } else if ($(this).val() == "heart js") {
+        $("label[for='color']").text("Color:");
+        $("#color option").show();
+        $("#color").val("tomato").show();
+        $("#color option[value='cornflowerblue']").hide();
+        $("#color option[value='darkslategrey']").hide();
+        $("#color option[value='gold']").hide();
+    } else {
+        $("label[for='color']").text("Please select a T-shirt theme");
+        $("#color").hide();
     }
 });
 
